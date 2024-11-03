2e515b90d552ebc924791fe9e508537b508e057f
Alexandre/Abashidze <abashidze.alexandre2012@gmail.com>
2024-11-03 19:55:30 +0400

    Add files via upload

---
+++ GOA-HOMEWORK/level 09/classwork/main.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+
+print(name + last_name + age)
+
+
+print("Alexandre" + "abashidze")
+print("foot" + "ball")
+print("basket" + "ball")
---
+++ GOA-HOMEWORK/level 09/homework/main.py
@@ -0,0 +1,49 @@
+#2
+country = "Georgia"
+weight = 12.5
+age = 12
+isAlien = False
+
+print("Type of country: ", country)
+print("Type of weight: ", weight)
+print("Type of age: ", age)
+print("Type of isAlien: ", isAlien)
+
+#3
+print(14 > 65)
+print(75 < 96)
+print(98 == 98)
+print(143 <= 132)
+print(789456 >= 789456)
+
+#4
+print(True and False)
+print(False and False)
+print(True and True)
+print(False or True)
+print(True or False)
+
+#5
+age = input("Enter your age: ")
+print("Type of age: ", type(age))
+
+#convert string to int
+age = int(age)
+print("Type of age: ", type(age))
+
+#convert var type to float
+age = float(age)
+print("Type of age: ", type(age))
+
+#6
+username = "Daviti"
+password = "World_123"
+
+name = input("Username: " )
+passw = input("Password: ")
+
+print("Is username correct: ", username == name)
+print("Is password correct: ", password == passw)
+
+#7
+#კონკატინაცია-არის ორი სტრინგის ერთმანეთზე დამატება
